a935bca903116b81a19f070418fd0b0db30b2cff
Crowdbotics <engineering@crowdbotics.com>
2023-06-11 23:21:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lucky-silence-dev-90482",
+  url: "https://lucky-silence-dev-90482.botics.co",
+  api: "https://lucky-silence-dev-90482.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
